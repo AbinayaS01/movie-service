b9a7827594bd3ba634ba6ac565b0ecbc8f8b622c
AbinayaS01 <spabinaya2003@gmail.com>
2025-01-09 16:05:02 +0530

    correct data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into movie_details(movie_id,movie_title,movie_language,movie_release_date,movie_image_url) values ('Conjuring','English','2014-07-14','');
-insert into movie_details(movie_id,movie_title,movie_language,movie_release_date,movie_image_url) values ('Avengers','English','2014-10-18','');
+insert into movie_details(movie_title,movie_language,movie_release_date,movie_image_url) values ('Conjuring','English','2014-07-14','');
+insert into movie_details(movie_title,movie_language,movie_release_date,movie_image_url) values ('Avengers','English','2014-10-18','');
